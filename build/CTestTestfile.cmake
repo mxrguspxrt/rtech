2205602906948455eb51432f90b35d4f60e83ae3
Margus Pärt <margus@wave.ee>
2019-09-22 17:23:53 +0000

    fixed untracked files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dte/Desktop/rtech/src
+# Build directory: /home/dte/Desktop/rtech/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CMAKE_PREFIX_PATH="/home/dte/Desktop/rtech/devel"
+export LD_LIBRARY_PATH=""
+export PKG_CONFIG_PATH=""
+export PYTHONPATH=""
+
+# modified environment variables
+export PWD="/home/dte/Desktop/rtech/build"
